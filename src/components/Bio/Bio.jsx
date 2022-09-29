66ed601de60bcc27686861fe88f625c8d1bfaad3
Emmanuel <emmychris477@gmail.com>
2022-09-29 12:56:48 +0100

    Implemented logic to populate name and about dynamically

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -1,16 +1,51 @@
+import { useState } from "react";
 import classes from "./Bio.module.css";
 import profileIcon from "../../assets/profileIcon.svg";
 
 const Bio = (props) => {
+  const [userDetails, setUserDetails] = useState({
+    name: "Nduka Emmanuel",
+    about: "Learning and growing in the tech space to become better!",
+  });
+
+  const [editformIsOpen, setEditFormIsOpen] = useState(false);
+
+  const updateUserDetails = (event) => {
+    event.preventDefault();
+    setUserDetails({
+      name: document.querySelector("#nameOfUser").value,
+      about: document.querySelector("#aboutUser").value,
+    });
+
+    setEditFormIsOpen(false);
+  };
+
+  const openEditForm = () => {
+    setEditFormIsOpen(true);
+  };
+
+  const closeEditForm = () => {
+    setEditFormIsOpen(false);
+  };
+
+  const editButton = <button onClick={openEditForm}>Edit</button>;
+
   const editForm = (
-    <form className={classes["edit-bio-form"]}>
-      <input type="text" id="" placeholder="Your Name"></input>
-      <input type="text" id="" placeholder="About You"></input>
+    <form
+      className={classes["edit-bio-form"]}
+      onSubmit={(e) => updateUserDetails(e)}
+    >
+      <input type="text" id="nameOfUser" placeholder="Your Name"></input>
+      <input type="text" id="aboutUser" placeholder="About You"></input>
       <br />
-      <button type="button" className={classes["cancel-button"]}>
+      <button
+        type="button"
+        className={classes["cancel-button"]}
+        onClick={closeEditForm}
+      >
         Cancel
       </button>
-      <button type="button">Save</button>
+      <button type="submit">Save</button>
     </form>
   );
 
@@ -24,12 +59,9 @@ const Bio = (props) => {
         <img src={profileIcon} alt="Profile"></img>
       </div>
       <div className={classes["profile-info"]}>
-        <p className={classes.name}>Nduka Emmanuel</p>
-        <p className={classes.about}>
-          Learning and growing in the tech space to become better!
-        </p>
-        {editForm}
-        <button>Edit</button>
+        <p className={classes.name}>{userDetails.name}</p>
+        <p className={classes.about}>{userDetails.about}</p>
+        {editformIsOpen ? editForm : editButton}
       </div>
     </section>
   );
