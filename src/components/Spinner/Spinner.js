f0ec17345f2695487ec457d15b5c93d2909f9005
Emmanuel <emmychris477@gmail.com>
2022-10-06 17:08:44 +0100

    spinner component

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,13 @@
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <>
+      <div className="card">
+        <div className="loader">Loading...</div>;
+      </div>
+    </>
+  );
+};
+
+export default Spinner;
